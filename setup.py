f2bd93b2ace3f98facaf7166336bb05a8dfaf794
William Tucker <william.tucker@stfc.ac.uk>
2017-02-28 11:00:55 +0000

    Updated version number.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except IOError:
 
 setup(
     name = 'ceda-pydap-utils',
-    version = '0.2.1',
+    version = '0.3.1',
     description = SHORT_DESCR,
     long_description = LONG_DESCR,
     author = 'William Tucker',
